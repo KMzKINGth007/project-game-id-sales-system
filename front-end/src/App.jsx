6499f8470b8ff05c633e16f003f9e969fb4e399e
KMzKINGth <643170010325@rmu.ac.th>
2024-02-25 04:40:07 +0700

    project

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -3,7 +3,7 @@ import AppRouter from "./routes/AppRouter";
 function App() {
 
   return (
-    <div className="min-h-screen">
+    <div className="main-page mx-auto">
       <AppRouter />
     </div>
   );
--- front-end/src/layout/Header.jsx
+++ front-end/src/layout/Header.jsx
@@ -22,26 +22,40 @@ export default function Header() {
   }
 
   return (
-    <div className="navbar bg-base-100">
-      <div className="navbar-start">
-        <a className="btn btn-ghost text-xl">Hello, {user?.id ? user.username : 'Guest'}</a>
-      </div>
-      <div className="navbar-center">
-        <a className="btn btn-ghost text-xl">LOGO</a>
-      </div>
-      <div className="navbar-end">
-        <ul className="menu menu-horizontal px-1">
-          {finalNav.map(el => (
-            <li key={el.to} >
-              <Link to={el.to}>{el.text}</Link>
-            </li>
-          ))}
+    <div className="header">
+      <div className='header-main'>
+        <div className="header-search">
+          <a className="">Hello, {user?.id ? user.username : 'Guest'}</a>
           {user?.id && (
-            <li>
-              <Link to='#' onClick={hdlLogout}>Logout</Link>
-            </li>
+            <div >
+              <input type="text" placeholder="Search..." />
+            </div>
           )}
-        </ul>
+        </div>
+        <div className="header-logo">
+          <a className="">LOGO</a>
+        </div>
+        <div className="header-menu">
+          <ul className="">
+            {finalNav.map(el => (
+              <li key={el.to} >
+                <Link to={el.to}>{el.text}</Link>
+              </li>
+            ))}
+            {user?.id && (
+              <li>
+                <Link to='#' onClick={hdlLogout}>Logout</Link>
+              </li>
+            )}
+          </ul>
+        </div>
+      </div>
+      <div className="header-bottom">
+        <div className="in-header-bottom">
+          <div className="in-header-bottom-center">
+            <ul><li>AAA</li><li>BBB</li><li>CCC</li></ul>
+            </div>
+        </div>
       </div>
     </div>
   );
